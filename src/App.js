d26f838f0cdf5c794f9ae2529112a703db558db9
Okabe-TO <ttt.sao.619@gmail.com>
2022-12-13 02:41:56 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import * as React from 'react'
-import './App.css'
-//import dogImage from 'https:\/\/images.dog.ceo\/breeds\/terrier-border\/n02093754_5975.jpg';
+import * as React from 'react';
+import State, { useState } from 'react';
+import './App.css';
 
 /**
  * 
  * @type {React.FC}
  */
+
 export const App = () => {
+  const [dogUrl] = useState(
+    "https:\/\/images.dog.ceo\/breeds\/terrier-border\/n02093754_5975.jpg"
+  );
   return (
     <div>
+      <title>ReactによるDogアプリ</title>
       <header>Dogアプリ</header>
       <p>
         犬の画像を表示するサイトです
       </p>
-      <img
-        src="https:\/\/images.dog.ceo\/breeds\/terrier-border\/n02093754_5975.jpg"
-        alt="dogImage"
-      />
+      <img src={dogUrl}/>
     </div>
   );
 }
