be0f093b5cccf16cff6ee2081a0c7ba2b9c795a1
Okabe-TO <ttt.sao.619@gmail.com>
2022-12-23 22:54:20 +0900

    station8.1

--- src/DogImage.js
+++ src/DogImage.js
@@ -4,12 +4,12 @@ import * as React from 'react'
 export const DogImage = (props) => {
   // 犬種を抽出
   const dogBreed = (props.url).split(/\/+/).slice(3, 4);
-  const dogUrl = props.url;
+  //const dogUrl = props.url;
   return(
     // ここのコンポーネントの抽出したい
     <>
       <img className='Image'
-        src={ dogUrl }
+        src={ props.url }
         alt={dogBreed}
       /><br />
       犬種 : { dogBreed }
